17b6683b657ca6b68011c35c02e63f775efb1f1c
Ralph <rdugue1@gmail.com>
2024-03-13 10:56:00 -0400

    gitflow-feature-stash: add-time

--- shared/src/commonMain/kotlin/data/ParkingSignsRepositoryImpl.kt
+++ shared/src/commonMain/kotlin/data/ParkingSignsRepositoryImpl.kt
@@ -4,6 +4,18 @@ import domain.entities.BitmapRequest
 import domain.entities.ParkingRequest
 import domain.entities.ParkingResponse
 import domain.repositories.ParkingSignsRepository
+import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
+import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.LocalTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.format
+import kotlinx.datetime.format.DateTimeComponents
+import kotlinx.datetime.format.DateTimeFormat
+import kotlinx.datetime.format.DateTimeFormatBuilder
+import kotlinx.datetime.format.FormatStringsInDatetimeFormats
+import kotlinx.datetime.format.byUnicodePattern
+import kotlinx.datetime.toLocalDateTime
 import kotlinx.serialization.json.Json
 
 class ParkingSignsRepositoryImpl(
@@ -30,7 +42,8 @@ class ParkingSignsRepositoryImpl(
     override suspend fun analyzeParkingSigns(request: BitmapRequest): Result<ParkingResponse> =
         try {
             val ocrResponse = ocrClient.analyzeParkingSigns(request.encodedBitmap, request.rotationDegrees)
-            val llmResponse = llmClient.generateResponse(formatPrompt(ocrResponse))
+            val formattedPrompt = formatPrompt(ocrResponse)
+            val llmResponse = llmClient.generateResponse(formattedPrompt)
             val parkingResponse = Json.decodeFromString<ParkingResponse>(llmResponse)
             Result.success(parkingResponse)
         } catch (e: Exception) {
@@ -56,8 +69,9 @@ class ParkingSignsRepositoryImpl(
      */
     private fun formatPrompt(ocrResponse: String): String {
         return """
-            Can I park here? The text from the parking signs say: $ocrResponse. 
-            If I can park, how long can I park for and how much does it cost?
+            Can I park here right now? It is ${getCurrentSystemTime()},
+            and the text from the parking signs say: $ocrResponse. 
+            If I can park, how long can I park? If there is a cost, how much does it cost?
             If I can't park, why not?
             Please respond in the following JSON format examples:
             {
@@ -73,5 +87,21 @@ class ParkingSignsRepositoryImpl(
                 "reasonIfNo": "No parking on Sundays"
             }
         """.trimIndent()
+    }
+
+    /**
+     * This function returns the current system time in the format of "HH:mm a".
+     * @return The current system time.
+     */
+    private fun getCurrentSystemTime(): String {
+        val now: Instant = Clock.System.now()
+        val localDateTime = now.toLocalDateTime(TimeZone.currentSystemDefault())
+        return LocalTime.Format {
+            amPmHour()
+            chars(":")
+            minute()
+            chars(" ")
+            amPmMarker("AM", "PM")
+        }.format(localDateTime.time)
     }
 }
