9389a5cf2ce7ad232b95d3aa789582d0b4fe9739
Ralph <rdugue1@gmail.com>
2024-03-17 14:02:47 -0400

    version bump

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -70,8 +70,8 @@ android {
         applicationId = "com.dugue.canipark"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 3
-        versionName = "1.1"
+        versionCode = 4
+        versionName = "1.2"
     }
     signingConfigs {
         create("release") {
