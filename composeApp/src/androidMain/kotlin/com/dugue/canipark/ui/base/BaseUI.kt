eccf81bb244c70f856a23147e55a435e53f69f4e
Ralph <rdugue1@gmail.com>
2024-05-21 09:51:21 -0400

    gitflow-hotfix-stash: ad-resize-fix

--- composeApp/src/androidMain/kotlin/com/dugue/canipark/ui/base/BaseUI.kt
+++ composeApp/src/androidMain/kotlin/com/dugue/canipark/ui/base/BaseUI.kt
@@ -17,6 +17,7 @@ import androidx.compose.material3.Text
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -37,23 +38,10 @@ fun LoadingDialog() {
             modifier = Modifier.fillMaxSize(),
             contentAlignment = Alignment.Center
         ) {
-            AndroidView(
-                modifier = Modifier.fillMaxWidth().align(Alignment.TopCenter).padding(8.dp),
-                factory = { context ->
-                    AdView(context).apply {
-                        adUnitId = "ca-app-pub-2138105660848240/7041466126"
-                        setAdSize(com.google.android.gms.ads.AdSize.BANNER)
-                        loadAd(AdRequest.Builder().build())
-                    }
-                },
-                update = { view ->
-                    // onAdViewReady(view)
-                }
-            )
             Card(
                 modifier = Modifier.padding(16.dp).align(Alignment.Center),
                 shape = RoundedCornerShape(8.dp),
-            ) { // Card to give a background color
+            ) {
                 Column(
                     modifier = Modifier.padding(16.dp),
                     horizontalAlignment = Alignment.CenterHorizontally
@@ -67,19 +55,6 @@ fun LoadingDialog() {
                     )
                 }
             }
-            AndroidView(
-                modifier = Modifier.fillMaxWidth().align(Alignment.BottomCenter).padding(8.dp),
-                factory = { context ->
-                    AdView(context).apply {
-                        adUnitId = "ca-app-pub-2138105660848240/1597567752"
-                        setAdSize(com.google.android.gms.ads.AdSize.BANNER)
-                        loadAd(AdRequest.Builder().build())
-                    }
-                },
-                update = { view ->
-                    // onAdViewReady(view)
-                }
-            )
         }
     }
 }
@@ -91,37 +66,73 @@ fun MessageDialog(
     onDismiss: () -> Unit
 ) {
     Dialog(onDismissRequest = { onDismiss() }) {
-        Card(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp),
-            shape = RoundedCornerShape(8.dp),
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
         ) {
-            Column(
-                modifier = Modifier.padding(8.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
+            AndroidView(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight()
+                    .align(Alignment.TopCenter),
+                factory = { context ->
+                    AdView(context).apply {
+                        adUnitId = "ca-app-pub-2138105660848240/7041466126"
+                        setAdSize(com.google.android.gms.ads.AdSize.BANNER)
+                        loadAd(AdRequest.Builder().build())
+                    }
+                },
+                update = { view ->
+                    // onAdViewReady(view)
+                }
+            )
+            Card(
+                modifier = Modifier
+                    .padding(8.dp),
+                shape = RoundedCornerShape(8.dp),
             ) {
-                Image(
-                    imageVector = if (isPositive) ImageVector.vectorResource(id = R.drawable.ic_check) else ImageVector.vectorResource(id = R.drawable.ic_error),
-                    contentDescription = "Result",
-                    modifier = Modifier.size(96.dp),
-                    colorFilter = ColorFilter.tint(
-                        if (isPositive) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.error
-                    )
-                )
-                Text(
+                Column(
                     modifier = Modifier.padding(8.dp),
-                    text = message,
-                    style = MaterialTheme.typography.bodyLarge,
-                    textAlign = TextAlign.Center
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-                Button(
-                    onClick = onDismiss
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Text("OK")
+                    Image(
+                        imageVector = if (isPositive) ImageVector.vectorResource(id = R.drawable.ic_check) else ImageVector.vectorResource(id = R.drawable.ic_error),
+                        contentDescription = "Result",
+                        modifier = Modifier.size(96.dp),
+                        colorFilter = ColorFilter.tint(
+                            if (isPositive) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.error
+                        )
+                    )
+                    Text(
+                        modifier = Modifier.padding(8.dp),
+                        text = message,
+                        style = MaterialTheme.typography.bodyLarge,
+                        textAlign = TextAlign.Center
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Button(
+                        onClick = onDismiss
+                    ) {
+                        Text("OK")
+                    }
                 }
             }
+            AndroidView(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight()
+                    .align(Alignment.BottomCenter),
+                factory = { context ->
+                    AdView(context).apply {
+                        adUnitId = "ca-app-pub-2138105660848240/1597567752"
+                        setAdSize(com.google.android.gms.ads.AdSize.BANNER)
+                        loadAd(AdRequest.Builder().build())
+                    }
+                },
+                update = { view ->
+                    // onAdViewReady(view)
+                }
+            )
         }
     }
 }
